1f172499bc3b545925c901a25057545baff52ee4
wtpbell <wtpbell@gmail.com>
2023-10-13 14:11:57 +0200

    cartSlice is done

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,42 @@
+export const addItem = (itemToAdd) => {
+    return {
+        type: 'cart/addItem',
+        payload: itemToAdd,
+    }
+}
+
+export const changeItemQuantity = (name,newQuantity) => {
+    return {
+        type: 'cart/changeItemQuantity',
+        payload: {name, newQuantity} ,
+    }
+}
+
+const initialCart = {};
+export const cartReducer = (cart = initialCart, {type, payload}) => {
+    switch (type){
+        case 'cart/addItem': {
+            const {name, price} = payload;
+            const quantity = cart[name]? cart[name].quantity + 1 : 1;
+            const newItem = {price, quantity};
+
+            return {
+                ...cart,
+                [name]: newItem
+            }}
+        case 'cart/changeItemQuantity':{
+            const {name, newQuantity} = payload;
+            const itemToUpdae = cart[name];
+            const updateItem = {
+                ...itemToUpdae,
+                quantity: newQuantity,
+            }
+            return {
+                ...cart, 
+                [name]: updateItem,
+            }}
+
+        default: 
+            return
+    }
+}
